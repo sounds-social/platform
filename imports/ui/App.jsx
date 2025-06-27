a731cf1362ee62615522e222dd594f8a60e26a17
Matteo Demicheli <matteodem@protonmail.com>
2025-06-27 15:27:49 +0200

    First commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = function () {
+  return <h2>Home</h2>;
+}
+
+const About = function () {
+  return <h2>About</h2>;
+}
+
+const Users = function () {
+  return <h2>Users</h2>;
+}
+
+export const App = function () {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
