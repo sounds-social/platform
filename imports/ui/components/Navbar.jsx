ba37e4bd89870e56408388ecce1bec62e97aa60f
Matteo Demicheli <matteodem@protonmail.com>
2025-07-02 18:02:34 +0200

    Only display notification button if user is logged in

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -88,12 +88,17 @@ const Navbar = ({ user }) => {
               </div>
             </div>
           </div>
+          
           <div className="hidden md:flex items-center space-x-2">
             {/* user && (
               <Link onClick={handleDropdownClick} to="/go-pro" className="text-blue-500 font-bold px-3 py-2 rounded-md text-sm mr-4">Go PRO</Link>
             ) */}
-            <Notifications />
-{userMenu}
+            {user && (
+              <>
+                <Notifications />
+                {userMenu}
+              </>
+            )}
           </div>
           <div className="-mr-2 flex md:hidden">
             <button onClick={() => setIsOpen(!isOpen)} className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
