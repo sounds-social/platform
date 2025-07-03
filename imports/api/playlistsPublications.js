e15029ca5e9b2e473afcfd2c149a61ac386d7c7d
Matteo Demicheli <matteodem@protonmail.com>
2025-07-03 18:49:37 +0200

    Only display playlists if they're public

--- imports/api/playlistsPublications.js
+++ imports/api/playlistsPublications.js
@@ -17,9 +17,21 @@ Meteor.publish('playlists.allPublicPlaylists', function publishAllPublicPlaylist
   return PlaylistsCollection.find({ isPublic: true });
 });
 
-Meteor.publish('playlists.singlePlaylist', function publishSinglePlaylist(playlistId) {
+Meteor.publish('playlists.singlePlaylist', async function publishSinglePlaylist(playlistId) {
   check(playlistId, String);
-  return PlaylistsCollection.find({ _id: playlistId });
+  const playlist = await PlaylistsCollection.findOneAsync({ _id: playlistId });
+
+  if (!playlist) {
+    return this.ready();
+  }
+
+  // If the playlist is public, or if the current user is the owner, publish it
+  if (playlist.isPublic || this.userId === playlist.ownerId) {
+    return PlaylistsCollection.find({ _id: playlistId });
+  } else {
+    // If it's private and not owned by the current user, don't publish
+    return this.ready();
+  }
 });
 
 Meteor.publish('playlists.publicPlaylistsForUser', function publishPublicPlaylistsForUser(userId) {
