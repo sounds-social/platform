69ff4d116d12195720d5aff352e86b77c9300604
Matteo Demicheli <matteodem@protonmail.com>
2025-07-04 16:12:29 +0200

    Add notifications button to mobile

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -100,8 +100,9 @@ const Navbar = ({ user }) => {
               </>
             )}
           </div>
-          <div className="-mr-2 flex md:hidden">
-            <button onClick={() => setIsOpen(!isOpen)} className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+          <div className="-mr-2 flex md:hidden items-center">
+            {user && <Notifications />}
+            <button onClick={() => setIsOpen(!isOpen)} className="ml-4 bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
               <span className="sr-only">Open main menu</span>
               {isOpen ? <FiX /> : <FiMenu />}
             </button>
