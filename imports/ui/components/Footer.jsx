d92f8129560a01b18b413bd2594fd378c47465dd
Matteo Demicheli <matteodem@protonmail.com>
2025-07-04 16:12:36 +0200

    Remove copyright icon from footer

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-white shadow-inner mt-8 py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <div className="text-gray-600 text-sm">
-          &copy; {new Date().getFullYear()} Sounds Social
+          {new Date().getFullYear()} Sounds Social
         </div>
         <div>
           <Link to="/terms-of-service" className="text-blue-500 hover:underline text-sm">
