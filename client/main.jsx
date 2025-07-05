2622326e944ef3a5d5de1a87b5ce10602a1c5168
Matteo Demicheli <matteodem@protonmail.com>
2025-07-05 18:08:42 +0200

    Remove head provider from main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -3,14 +3,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { AppWithAudioPlayerProvider } from '/imports/ui/App';
-import { HeadProvider } from 'react-head';
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
-  root.render(
-    <HeadProvider>
-      <AppWithAudioPlayerProvider />
-    </HeadProvider>
-  );
+  root.render(<AppWithAudioPlayerProvider />);
 });
