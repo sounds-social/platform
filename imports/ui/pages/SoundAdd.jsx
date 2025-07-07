50640e86887e5c99f900f44511993c5877c18bb2
Matteo Demicheli <matteodem@protonmail.com>
2025-07-07 18:43:22 +0200

    Revert to accept any audio files

--- imports/ui/pages/SoundAdd.jsx
+++ imports/ui/pages/SoundAdd.jsx
@@ -111,7 +111,7 @@ const SoundAdd = () => {
 
             <div>
               <label htmlFor="audioFile" className="block text-sm font-medium text-gray-700">Audio File</label>
-              <UploadcareWidget onUpload={setAudioFile} accept="audio/mp3" />
+              <UploadcareWidget onUpload={setAudioFile} accept="audio/*" />
               {audioFile && (
                 <audio controls src={audioFile}></audio>
               )}
