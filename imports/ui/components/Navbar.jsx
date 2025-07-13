e24d9506874d2ddf8ffc41f5b7c980f5db0469c1
Matteo Demicheli <matteodem@protonmail.com>
2025-07-13 15:24:59 +0200

    Only display support overview on mobile if user is on pro plan

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -141,7 +141,9 @@ const Navbar = ({ user }) => {
           {user && (
             <div className="pt-4 pb-3 border-t border-gray-200">
               <div className="px-2 space-y-1">
-                <Link onClick={handleDropdownClick} to="/support-overview" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700">Support Overview</Link>
+                {user && user.plan === 'pro' && (
+                  <Link onClick={handleDropdownClick} to="/support-overview" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700">Support Overview</Link>
+                )}
                 <Link onClick={handleDropdownClick} to="/payouts" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700">Payouts</Link>
                 <Link onClick={handleDropdownClick} to="/explore" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700">Explore</Link>
                 <Link onClick={handleDropdownClick} to="/profile" className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700">Profile</Link>
