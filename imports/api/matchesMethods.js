2bde69ffca5d852650351eadaac0714011989678
Matteo Demicheli <matteodem@protonmail.com>
2025-07-25 15:57:01 +0200

    Create notifications for both users when they match

--- imports/api/matchesMethods.js
+++ imports/api/matchesMethods.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Matches } from './matches';
 import { Sounds } from './sounds';
+import { Notifications } from './notifications';
 
 Meteor.methods({
   async 'matches.swipe'({ swipedUserId, direction }) {
@@ -49,6 +50,23 @@ Meteor.methods({
           },
           { multi: true }
         );
+
+        const currentUser = await Meteor.users.findOneAsync(this.userId);
+        const swipedUser = await Meteor.users.findOneAsync(swipedUserId);
+
+        if (currentUser && swipedUser) {
+          await Notifications.insertAsync({
+            userId: this.userId,
+            message: `You matched with ${swipedUser.profile.firstName}! Send them a DM to get started`,
+            link: `/profile/${swipedUser.profile.slug}`,
+          });
+
+          await Notifications.insertAsync({
+            userId: swipedUserId,
+            message: `You matched with ${currentUser.profile.firstName}! Send them a DM to get started`,
+            link: `/profile/${currentUser.profile.slug}`,
+          });
+        }
 
         return { matched: true, swipedUserId };
       }
