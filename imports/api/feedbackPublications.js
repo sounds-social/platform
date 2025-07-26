52eeba375b52302bef86abe13787064221ca7e97
Matteo Demicheli <matteodem@protonmail.com>
2025-07-26 18:16:19 +0200

    Display sound title for feedbacks received

--- imports/api/feedbackPublications.js
+++ imports/api/feedbackPublications.js
@@ -8,6 +8,18 @@ Meteor.publish('feedback.forUser', async function () {
     return this.ready();
   }
   const userSoundIds = await Sounds.find({ userId: this.userId }).map(s => s._id);
+  const feedbacks = await Feedback.find({ soundId: { $in: userSoundIds } }, { sort: { createdAt: -1 } });
 
-  return Feedback.find({ soundId: { $in: userSoundIds } }, { sort: { createdAt: -1 } });
+  feedbacks.forEach(async (feedback) => {
+    const sound = await Sounds.findOneAsync(feedback.soundId);
+    if (sound) {
+      this.added('sounds', sound._id, sound);
+    }
+    const giver = await Meteor.users.findOneAsync(feedback.giverId);
+    if (giver) {
+      this.added('users', giver._id, { profile: giver.profile });
+    }
+  });
+
+  return feedbacks;
 });
