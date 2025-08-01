5a1a3b2c25f99194e2d223a81595a77cec5ea32f
Matteo Demicheli <matteodem@protonmail.com>
2025-08-01 17:14:35 +0200

    Add discord link to footer

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -9,6 +9,9 @@ const Footer = () => {
           {new Date().getFullYear()} Sounds Social
         </div>
         <div>
+          <a href="https://discord.gg/Je3P3gQ9u5" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline text-sm mr-4">
+            Discord
+          </a>
           <Link to="/about" className="text-blue-500 hover:underline text-sm mr-4">
             About
           </Link>
